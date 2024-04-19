bea540f02531d8b4b058817d743fd753395401cd
Bacem <bacemjarraya@yahoo.fr>
2024-04-19 11:02:23 +0100

    t13-3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,19 +4,30 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string({
-    invalid_type_error: 'Please select a customer.',
-  }),
-  amount: z.coerce.number().gt(
-    0,{message: 'Please enter a valid amount greater than $0.'}
-  ),
-  status: z.enum(['pending', 'paid'],{
-    invalid_type_error: 'Please select an invoice status.',
-  }),
-  date: z.string(),
-});
+const FormSchema = z.object(
+  {
+    id: z.string(),
+    customerId: z.string(
+      {
+        invalid_type_error: 'Please select a customer.',
+      }
+    ),
+    amount: z.coerce.number().gt(
+      0,
+      {
+        message: 'Please enter a valid amount greater than $0.'
+      }
+    ),
+    status: z.enum(
+      [
+        'pending', 'paid'
+      ],{
+        invalid_type_error: 'Please select an invoice status.',
+      }
+    ),
+    date: z.string(),
+  }
+);
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
